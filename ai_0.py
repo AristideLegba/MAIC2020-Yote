14528c70d4555a8e83539511f819069b46ea86d5
Aristide legba <aristidelegba67@gmail.com>
2020-11-12 15:59:18 +0100

    apport de modification

--- ai_0.py
+++ ai_0.py
@@ -1,15 +1,18 @@
 from core import Player
 from yote.yote_rules import YoteRules
+from core import Color
+from yote.yote_action import YoteActionType, YoteAction
 import json
-
+import numpy as np
 
 class DeusExMachinaInterface(object):
-    def __init__(self, state, remain_time):
+    def __init__(self, state, remain_time, position):
         self.state = state
         self.remain_time = remain_time
+        self.position = position
 
     def get_board(self):
-        print(json.loads(self.state.get_json_state())['board'])
+        return json.loads(self.state.get_json_state())['board']
 
     def get_latest_player(self):
         print(self.state.get_latest_player())
@@ -24,15 +27,34 @@ class DeusExMachinaInterface(object):
         # return state.
 
     def get_player_info(self):
-        print(self.state.get_player_info(get_latest_player()))
+        print(self.state.get_player_info(self.position))
         # return state.
 
     def get_enemy_info(self):
-        print(self.state.get_player_info(get_latest_player()))
+        print(self.state.get_player_info( -1 * self.position ))
         # return state.
 
     def get_json_state(self):
-        print(self.state.get_json_state())
+        return [tuple(cell) for cell in np.argwhere(self.get_board())]
+        # print(self.state.get_json_state())
+        # return state.
+
+    def add_piece(self, to):
+        print(self.remain_time)
+        # return state.
+
+    def move_piece(self, moveFrom, to):
+        print(self.remain_time)
+        # return state.
+
+
+    def steal_from_board(self, at):
+        print(self.remain_time)
+        # return state.
+
+
+    def steal_from_hand(self):
+        print(self.remain_time)
         # return state.
 
 
@@ -46,7 +68,8 @@ class AI(Player):
         self.start_game = False
 
     def play(self, state, remain_time):
-        # print("time remain is ", remain_time, " seconds")
-        gameInfos = DeusExMachinaInterface(state, remain_time)
-        gameInfos.get_board()
+        gameInfos = DeusExMachinaInterface(state, remain_time, self.position)
+        # gameInfos.get_board()
+        
+        print(gameInfos.get_json_state())
         return YoteRules.random_play(state, self.position)
